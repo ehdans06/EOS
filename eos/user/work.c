a1ccfff91964ac4b9e02ec026c5648154278219d
민동문\민 동문 <ehdans06@gmail.com>
2017-05-31 15:04:34 +0900

    a

--- eos/user/work.c
+++ eos/user/work.c
@@ -31,7 +31,6 @@ static void receiver_task1(void* arg) {
 	while(1) {
 		PRINT("receive message from mq1\n");
 		eos_receive_message(&mq1, data, 0);
-		PRINT("A");
 		PRINT("received message: %s\n", data);
 		eos_sleep(0);
 	}
@@ -54,8 +53,8 @@ void eos_user_main() {
 	eos_create_task(&tcb3, (addr_t)stack3, 8096, receiver_task2, NULL, 10);
 
 	eos_set_period(&tcb1, 2);
-	eos_set_period(&tcb1, 4);
-	eos_set_period(&tcb1, 5);
+	eos_set_period(&tcb2, 4);
+	eos_set_period(&tcb3, 5);
 
 	eos_init_mqueue(&mq1, queue1, 5, 2, FIFO);
 	eos_init_mqueue(&mq2, queue2, 5, 2, FIFO);
